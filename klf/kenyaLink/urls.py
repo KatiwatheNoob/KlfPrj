17972ac2e0e3c0113613e6fbe9add215b3108454
Katiwa <roykatiwa@gmail.com>
2024-04-16 09:01:33 +0300

    Update 1.1

--- klf/kenyaLink/urls.py
+++ klf/kenyaLink/urls.py
@@ -14,12 +14,31 @@ urlpatterns = [
     path('permits', views.permits, name = 'permits'),
     path('passes', views.passes, name = 'passes'),
     path('visas', views.visas, name = 'visas'),
-    path('permanentresidency', views.permanentresidency, name = 'permanentresidency'),
+    path('permanent-residency', views.permanentResidency, name = 'permanent-residency'),
     path('citizenship', views.citizenship, name = 'citizenship'),
-    #path('contact', views.contact, name = 'contact'),
-    #path('contact', views.contact, name = 'contact'),
-    #path('contact', views.contact, name = 'contact'),
-    #path('contact', views.contact, name = 'contact'),
+
+    #permits
+    path('class-c', views.classC, name = 'class-c'),
+    path('class-d', views.classD, name = 'class-d'),
+    path('class-g', views.classG, name = 'class-g'),
+    path('class-i', views.classI, name = 'class-i'),
+    path('class-k', views.classK, name = 'class-k'),
+    path('class-m', views.classM, name = 'class-m'),
+
+    #passes
+
+    path('dependant-pass', views.dependantPass, name = 'dependant-pass'),
+    path('extension-pass', views.extensionPass, name = 'extension-pass'),
+    path('student-pass', views.studentPass, name = 'student-pass'),
+    path('special-pass', views.specialPass, name = 'special-pass'),
+    path('re-entry-pass', views.reEntryPass, name = 're-entry-pass'),
+
+    #permanent residency
+    path('category-a', views.categoryA, name = 'category-a'),
+    path('category-b', views.categoryB, name = 'category-b'),
+    path('category-c ', views.categoryC, name = 'category-c'),
+    path('category-d', views.categoryD, name = 'category-d'),
+
 
 ]
 
--- klf/kenyaLink/views.py
+++ klf/kenyaLink/views.py
@@ -4,25 +4,69 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'base/home.html')
 
 def contact(request):
-    return render(request, 'contact.html')
+    return render(request, 'base/contact.html')
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'base/about.html')
 
 def faqs(request):
-    return render(request, 'faqs.html')
+    return render(request, 'base/faqs.html')
 
+#sevices
 def permits(request):
-    return render(request, 'permits.html')
+    return render(request, 'services/permits.html')
+
 def passes(request):
-    return render(request, 'passes.html')
+    return render(request, 'services/passes.html')
+
 def visas(request):
-    return render(request, 'visas.html')
-def permanentresidency(request):
-    return render(request, 'permanentresidency.html')
+    return render(request, 'services/visas.html')
+
+def permanentResidency(request):
+    return render(request, 'services/permanent-residency.html')
+
 def citizenship(request):
-    return render(request, 'citizenship.html')
+    return render(request, 'services/citizenship.html')
+
+#permits
+def classC(request):
+    return render(request, 'permits/class-c.html')
+def classD(request):
+    return render(request, 'permits/class-d.html')
+def classG(request):
+    return render(request, 'permits/class-g.html')
+def classI(request):
+    return render(request, 'permits/class-i.html')
+def classK(request):
+    return render(request, 'permits/class-k.html')
+def classM(request):
+    return render(request, 'permits/class-m.html')
+
+#passes
+def dependantPass(request):
+    return render(request, 'passes/dependant-pass.html')
+def extensionPass(request):
+    return render(request, 'passes/extension-pass.html')
+def reEntryPass(request):
+    return render(request, 'passes/re-entry-pass.html')
+def specialPass(request):
+    return render(request, 'passes/special-pass.html')
+def studentPass(request):
+    return render(request, 'passes/student-pass.html')
+
+#permanet residency
+def categoryA(request):
+    return render(request, 'permanent-residency/category-a.html')
+def categoryB(request):
+    return render(request, 'permanent-residency/category-b.html')
+def categoryC(request):
+    return render(request, 'permanent-residency/category-c.html')
+def categoryD(request):
+    return render(request, 'permanent-residency/category-d.html')
+
+
+
 
